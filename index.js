7d58b00db2e4660e250678bdb28f1c15d884c591
caroline opiyo <opiyocaroline20@gmail.com>
2023-09-30 17:32:24 +0300

    phase-0-javascript-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft(){
+  const leftNum = dodger.style.left.replace("px", "")
+  const left = parseInt(leftNum, 10)
+
+  if(left > 0){
+      dodger.style.left = `${left-1}px`
+  }
+}
+
+function moveDodgerRight(){
+  const rightNum = dodger.style.left.replace("px", "");
+  const right  = parseInt(rightNum);
+
+  if(right > 0){
+      dodger.style.left = `${right+1}px`
+  }
+}
